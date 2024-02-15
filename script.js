ced21130e81f7ab17bdbdc584be7c67bb7ad5504
Pengpa0012 <peng.ilikha@gmail.com>
2024-02-15 17:15:46 +0800

    added api

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 const map = L.map('map').setView([14.639270610701264, 120.97380513145693], 13)
 const loader = document.querySelector("#loader")
+let markerLists = []
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map)
 
 map.on('click', function(e) {        
-	const popLocation= e.latlng;
-	console.log(popLocation)
-	loader.classList.remove("pointer-none", "invisible")
-	setTimeout(() => {
-		loader.classList.add("pointer-none", "invisible")
-	}, 1000)
-	const marker = L.marker([14.639270610701264, 120.97380513145693]).addTo(map)
-	marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup()
+	const loc = e.latlng
 
+	// Loader
+	loader.classList.remove("pointer-none", "invisible")
+
+	const url = `https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=${loc.lat}%2C%20${loc.lng}&radius=500&language=en`
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'API-KEY',
+			'X-RapidAPI-Host': 'API-HOST'
+		}
+	}
+
+	fetch(url, options)
+	.then(res => res.json())
+	.then(data => {
+		console.log(data.results)
+		data.results.forEach((el, i) => {
+			// Set pin
+			const marker = L.marker([el.location.lat, el.location.lng]).addTo(map)
+			// add name of store
+			marker.bindPopup(el.name).openPopup()
+
+			const markerExists = markerLists.some(coord => (
+					coord.lat === el.location.lat && coord.lng === el.location.lng
+			));
+
+			// check if coordinate is not added
+			if (!markerExists) {
+					const marker = L.marker([el.location.lat, el.location.lng]).addTo(map)
+					marker.bindPopup(el.name).openPopup()
+					markerLists.push({
+							lat: el.location.lat,
+							lng: el.location.lng
+					})
+			}
+		})
+		loader.classList.add("pointer-none", "invisible")
+	})
 })
